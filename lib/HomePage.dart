5083b116cc534fa8b6df1bde256dbb4cc75ecd83
Ahmad Fitri Bin San Ahmad <fitrisan@TDC.TIME.ORG>
2019-12-30 09:40:06 +0800

    added icons on left side bar menu

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -21,6 +21,25 @@ class _HomeState extends State<Home> {
         ]
       ),
 
+      drawer : new Drawer(
+        child: new ListView(
+          children : <Widget>[
+            new UserAccountsDrawerHeader(accountName: new Text("Code With Newswav"), accountEmail: new Text("piteli@gmail.com"),
+              decoration: new BoxDecoration(color : Colors.purple)),
+            new ListTile(title : new Text("Home Page") , leading: new Icon(Icons.cake, color: Colors.purple,),),
+            new ListTile(title : new Text("News Page"), leading : new Icon(Icons.search, color: Colors.redAccent)),
+            new ListTile(title : new Text("Third Page")),
+            new ListTile(title : new Text("Third Page"), leading : new Icon(Icons.cached, color : Colors.orange)),
+            new Divider(height: 10.0, color : Colors.black),
+            new ListTile(title : new Text("Close"), trailing: new Icon(Icons.close, color : Colors.red),
+              onTap: (){
+              Navigator.of(context).pop();
+              },)
+          ]
+        ),
+      )
+
+
     );
   }
 }
