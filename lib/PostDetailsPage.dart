ad61ffc654347c403d8a9d0141381c0070eecb6e
piteli <rushwender@gmail.com>
2020-01-10 15:23:47 +0800

    Updated

--- lib/PostDetailsPage.dart
+++ lib/PostDetailsPage.dart
@@ -14,6 +14,29 @@ class PostDetails extends StatefulWidget {
 class _PostDetailsState extends State<PostDetails> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return new Scaffold(
+      appBar : new AppBar(
+        title : new Text("Post Details"),
+        backgroundColor: Colors.green,
+
+      ),
+      body : new Card(
+        elevation : 10.0,
+        margin : EdgeInsets.all(10.0),
+        child : new ListView(
+          children : <Widget>[
+            new Row(
+              children : <Widget>[
+                new CircleAvatar(
+                  child : new Text(widget.snapshot.data["title"][0]),
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                )
+              ]
+            )
+          ]
+        )
+      )
+    );
   }
 }
