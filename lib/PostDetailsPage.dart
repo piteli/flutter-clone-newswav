3fca1fb9adf6962efa3d28e34e7d26d835e9a43d
piteli <rushwender@gmail.com>
2020-01-14 14:56:51 +0800

    Updated

--- lib/PostDetailsPage.dart
+++ lib/PostDetailsPage.dart
@@ -25,14 +25,25 @@ class _PostDetailsState extends State<PostDetails> {
         margin : EdgeInsets.all(10.0),
         child : new ListView(
           children : <Widget>[
-            new Row(
-              children : <Widget>[
-                new CircleAvatar(
-                  child : new Text(widget.snapshot.data["title"][0]),
-                  backgroundColor: Colors.green,
-                  foregroundColor: Colors.white,
+            new Container(
+              padding : EdgeInsets.all(10.0),
+                child : Row(
+                    children : <Widget>[
+                      new CircleAvatar(
+                        child : new Text(widget.snapshot.data["title"][0]),
+                        backgroundColor: Colors.green,
+                        foregroundColor: Colors.white,
+                      ),
+                      new SizedBox(width : 10.0,),
+                      new Text(widget.snapshot.data["title"],
+                          style : TextStyle(fontSize: 22.0, color : Colors.orange))
+                    ]
                 )
-              ]
+            ),
+            new SizedBox(height : 7.0),
+            new Container(
+              margin : EdgeInsets.all(10.0),
+              child : new Text(widget.snapshot.data["content"], style : TextStyle(fontSize: 18.0))
             )
           ]
         )
